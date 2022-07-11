7b7e54b4b5ca749c8233030d1592bc2603cff322
AshMW <contact@ashmw.com>
2022-07-11 11:09:12 -0400

    fix(superBoxStyle): chakra v2

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ import {
   Link,
   LinkBox,
   LinkBoxProps,
-  LinkOverlay
+  LinkOverlay,
 } from '@chakra-ui/react';
 import Head from 'next/head';
 
@@ -24,7 +24,7 @@ export default function IndexPage() {
     border: '2px solid',
     borderColor: 'white',
     transition: 'all 0.2s',
-    isTruncated: true,
+    noOfLines: 1,
     position: 'relative',
     _before: {
       content: '"→"',
@@ -35,64 +35,64 @@ export default function IndexPage() {
       bg: 'white',
       position: 'absolute',
       right: 5,
-      top: 2.5
+      top: 2.5,
     },
     _hover: {
       borderColor: 'pink.400',
       color: 'pink.400',
       shadow: 'md',
-      _before: { opacity: 1, right: 3 }
-    }
-  }
-  return <>
-    <Head>
-      <title>Success!</title>
-    </Head>
-    <Flex
-      align="center"
-      justify="center"
-      minH="100vh"
-      w="100vw"
-      bg="gray.100"
-    >
-      <Box maxW="2xl" w="100%" p={2}>
-        <Box shadow="sm" bg="white" py={5} px={6} rounded="md" w="100%">
-          <Heading color="pink.400">Success!</Heading>
-          <Text fontSize="lg" fontWeight="500">You are now using <Badge>Next.JS</Badge> + <Badge colorScheme="blue">TypeScript</Badge> + <Badge colorScheme="green">Chakra UI</Badge></Text>
+      _before: { opacity: 1, right: 3 },
+    },
+  };
+  return (
+    <>
+      <Head>
+        <title>Success!</title>
+      </Head>
+      <Flex align="center" justify="center" minH="100vh" w="100vw" bg="gray.100">
+        <Box maxW="2xl" w="100%" p={2}>
+          <Box shadow="sm" bg="white" py={5} px={6} rounded="md" w="100%">
+            <Heading color="pink.400">Success!</Heading>
+            <Text fontSize="lg" fontWeight="500">
+              You are now using <Badge>Next.JS</Badge> + <Badge colorScheme="blue">TypeScript</Badge> +{' '}
+              <Badge colorScheme="green">Chakra UI</Badge>
+            </Text>
+          </Box>
+          <SimpleGrid mt={5} columns={2} spacing={5}>
+            <LinkBox {...superBoxStyle}>
+              <LinkOverlay isExternal href="https://reactjs.org/docs/getting-started.html">
+                React Documentation
+              </LinkOverlay>
+            </LinkBox>
+            <LinkBox {...superBoxStyle}>
+              <LinkOverlay isExternal href="https://nextjs.org/docs">
+                Next.JS Documentation
+              </LinkOverlay>
+            </LinkBox>
+            <LinkBox {...superBoxStyle}>
+              <LinkOverlay isExternal href="https://chakra-ui.com/docs">
+                Chakra UI Documentation
+              </LinkOverlay>
+            </LinkBox>
+            <LinkBox {...superBoxStyle}>
+              <LinkOverlay isExternal href="https://www.typescriptlang.org/docs/">
+                TypeScript Documentation
+              </LinkOverlay>
+            </LinkBox>
+          </SimpleGrid>
+          <Link
+            display="block"
+            color="gray.500"
+            mt={10}
+            fontSize="sm"
+            textAlign="right"
+            href="https://github.com/AshMW2724/nextjs-typescript-chakra"
+            isExternal
+          >
+            Make sure to ⭐ the repository on GitHub!
+          </Link>
         </Box>
-        <SimpleGrid mt={5} columns={2} spacing={5}>
-          <LinkBox {...superBoxStyle}>
-            <LinkOverlay isExternal href="https://reactjs.org/docs/getting-started.html">
-              React Documentation
-            </LinkOverlay>
-          </LinkBox>
-          <LinkBox {...superBoxStyle}>
-            <LinkOverlay isExternal href="https://nextjs.org/docs">
-              Next.JS Documentation
-            </LinkOverlay>
-          </LinkBox>
-          <LinkBox {...superBoxStyle}>
-            <LinkOverlay isExternal href="https://chakra-ui.com/docs">
-              Chakra UI Documentation
-            </LinkOverlay>
-          </LinkBox>
-          <LinkBox {...superBoxStyle}>
-            <LinkOverlay isExternal href="https://www.typescriptlang.org/docs/">
-              TypeScript Documentation
-            </LinkOverlay>
-          </LinkBox>
-        </SimpleGrid>
-        <Link
-          display="block"
-          color="gray.500"
-          mt={10} fontSize="sm"
-          textAlign="right"
-          href="https://github.com/AshMW2724/nextjs-typescript-chakra"
-          isExternal
-        >
-          Make sure to ⭐ the repository on GitHub!
-        </Link>
-      </Box>
-    </Flex>
-  </>
+      </Flex>
+    </>
+  );
 }
